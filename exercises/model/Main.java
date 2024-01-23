8c89034047a2f101638706d25df4878cfe1ad857
A. Tsatsouli <tantonia01@gmail.com>
2024-01-23 10:24:37 +0200

    Reflection

--- exercises/model/Main.java
+++ exercises/model/Main.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package gr.aueb.cf.exercises.model;
 
 public class Main {
@@ -12,22 +11,4 @@ public class Main {
         System.out.println("Credential id is: " + userCredentials.getId() + " username is: " + userCredentials.getUserName()
                 + " and password is: " + userCredentials.getPassword());
     }
-
 }
-=======
-package gr.aueb.cf.exercises.model;
-
-public class Main {
-
-    public static void main(String[] args) {
-        User user = new User(124525859L, "Antonia", "T.");
-        UserCredentials userCredentials = new UserCredentials(25252525L, "anto", "AntTsa");
-
-        System.out.println("User id is: " + user.getId() + " , firstname is: " + user.getFirstname() + " , lastname is: "
-        + user.getLastname());
-        System.out.println("Credential id is: " + userCredentials.getId() + " username is: " + userCredentials.getUserName()
-                + " and password is: " + userCredentials.getPassword());
-    }
-
-}
->>>>>>> d5544524851e3e881f020f73f789d61cf060d752
